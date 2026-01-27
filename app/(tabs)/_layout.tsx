663e77a09dd517c00648f530093cacd4a7770a6c
peterkruhoffer <peter.kruhoffer@gmail.com>
2026-01-27 19:32:50 +0100

    Setup tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol.ios";
+import { Tabs } from "expo-router";
+import { View } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: () => <View className="bg-black h-full w-full" />,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="progress"
+        options={{
+          title: "Progress",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="chart.bar.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
